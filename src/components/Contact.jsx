67274c059e7e128fd2ed18267f3ad1be3da95084
adylitto <adylitto@gmail.com>
2023-03-04 12:48:52 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Adyl.dev",
           from_email: form.email,
           to_email: "adylitto@gmail.com",
           message: form.message,
